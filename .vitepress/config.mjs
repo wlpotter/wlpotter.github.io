b40e12d8ea714da31747ece076401dcd268e7667
William L Potter <44096602+wlpotter@users.noreply.github.com>
2025-04-11 15:30:33 -0500

    Update navigation configs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -8,21 +8,36 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'About', link: '/about'},
+      { text: 'Projects', link: '/projects/'},
+      { text: 'CV', link: '/cv' }
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: 'Projects',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: 'Sinai Manuscripts Data Portal', link: '/projects/smdp' },
+          { text: 'Syriac Manuscripts in the British Library', link: '/projects/smbl' },
+          { text: 'The Syriac Gazeteer', link: '/projects/smdp' },
+          { text: 'The Comprehensive Bibliography on Syriac Studies', link: '/projects/cbss' },
+          { text: 'Caesarea-Maritima.org: A Digital Archive', link: '/projects/caesarea' },
+          { text: 'Sinai Manuscripts Digital Library', link: '/projects/smdl' },
+          { text: 'The Beth Qaṭraye Gazetteer', link: '/projects/bqg' },
+          { text: 'Linked Data for Syriac Manuscripts', link: '/projects/linked-mss' }
+        ]
+      },
+      {
+        text: 'About',
+        items: [
+          { text: 'About Me', link: '/about' },
+          { text: 'Curriculum Vitae', link: '/cv' }
         ]
       }
     ],
-
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/wlpotter' },
+      { icon: 'linkedin', link: 'https://www.linkedin.com/in/williamlpotter/' }
     ]
   }
 })
